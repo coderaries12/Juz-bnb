1a915224cadaf0c8a90823a1a65d8c269c1b8420
Sheena <aries.sheen@gmail.com>
2023-04-14 09:20:57 -0700

    Options object added in migration file

--- backend/db/migrations/20230413195437-create-user.js
+++ backend/db/migrations/20230413195437-create-user.js
@@ -49,7 +49,8 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+
+    },options);
   },
   async down(queryInterface, Sequelize) {
     options.tableName='Users';
