52e181f1ab5a8e1ceb45f955f655d51a2a08ca67
Sheena <aries.sheen@gmail.com>
2023-04-13 18:04:05 -0700

    Added firstName and lastName to models,migration file and modify the routes

--- backend/db/migrations/20230413195437-create-user.js
+++ backend/db/migrations/20230413195437-create-user.js
@@ -15,6 +15,15 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      firstName:{
+       type:Sequelize.STRING,
+       allowNull:false
+      },
+      lastName:{
+        type:Sequelize.STRING,
+        allowNull:false
+
+      },
       username: {
         type: Sequelize.STRING(30),
         allowNull:false,
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -29,17 +29,31 @@ const validateSignup = [
     handleValidationErrors
   ];
 
+// router.post('/api/users',async(req,res)=>{
+//   const{ firstName, lastName, email, userName}=req.body;
+//   const newUser=await User.create({
+//     firstName,
+//     lastName,
+//     email,
+//     userName
+//   })
+// return res.json(newUser)  
+
+// })  
+
 // Sign up
 router.post(
     '/',
     validateSignup,
     async (req, res) => {
-      const { email, password, username } = req.body;
+      const { firstName,lastName,email, password, username } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ email, username, hashedPassword });
+      const user = await User.create({ firstName,lastName,email, username, hashedPassword });
   
       const safeUser = {
         id: user.id,
+        firstName:user.firstName,
+        lastName:user.lastName,
         email: user.email,
         username: user.username,
       };
