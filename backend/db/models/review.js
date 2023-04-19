4a41d825a1053ad286844b26023fdf0f91b54d59
Sheena <aries.sheen@gmail.com>
2023-04-18 21:41:48 -0700

    Added route Add an image to spot and Get all reviews by a spot and also added id primary key in model files

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -19,6 +19,12 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Review.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     spotId:{
       type: DataTypes.INTEGER,
       references:{
--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -17,6 +17,12 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   SpotImage.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     spotId: {
       type:DataTypes.INTEGER,
       references:{
