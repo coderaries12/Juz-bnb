45a0b2f9299437744f20e916ed42d1796e66c69f
Sheena <aries.sheen@gmail.com>
2023-05-04 21:52:39 -0700

    Frontend Phase-1 completed

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,11 @@
 // frontend/src/store/index.js
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import sessionReducer from "./session";
+
+
 const rootReducer = combineReducers({
+    session: sessionReducer, 
 });
 
 let enhancer;
@@ -17,5 +21,6 @@ if (process.env.NODE_ENV === 'production') {
 const configureStore = (preloadedState) => {
     return createStore(rootReducer, preloadedState, enhancer);
   };
-  
+
+
   export default configureStore;
