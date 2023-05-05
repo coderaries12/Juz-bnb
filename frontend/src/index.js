780549a6a84cbb1956a86767f76dcab8f18979e7
Sheena <aries.sheen@gmail.com>
2023-05-05 14:02:21 -0700

    Phase-4done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ModalProvider, Modal } from './context/Modal';
 import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from "./store/session";
+
 
 
 
@@ -22,11 +24,14 @@ if (process.env.NODE_ENV !== 'production') {
 
 function Root() {
   return (
+    <ModalProvider>
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <Modal />
       </BrowserRouter>
     </Provider>
+    </ModalProvider>
   );
 }
 
